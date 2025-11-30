9c2b2aa39fdd71b85a86bc5a7c76c62684b0d198
Novak Istvan <novak.istvan.ifj@gmail.com>
2025-11-30 14:17:27 +0100

    feature branch megváltozott üzenete!!!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello DevOps!! Atreides!!");
+    res.send("Ismét egy megváltozott üzenet!! Muadib");
 });
 
 app.listen(8080, () => {
