bbd29c6796ab15cf9e63cfc4cb4a32337ff4d3ca
Novak Istvan <novak.istvan.ifj@gmail.com>
2025-11-30 16:10:06 +0100

    üzenet megváltoztatása

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Megváltozott üzenet ez már!!");
+    res.send("Ismét egy megváltozott üzenet!! Muadib");
 });
 
 app.listen(8080, () => {
