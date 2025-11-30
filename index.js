7c98277a51fbc2ca01b4dc14b5891a7dd1391ca4
Novak Istvan <novak.istvan.ifj@gmail.com>
2025-11-30 14:09:50 +0100

    kiírás változtatás az index.js-ben

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello DevOps World!");
+    res.send("Hello DevOps!! Atreides!!");
 });
 
 app.listen(8080, () => {
